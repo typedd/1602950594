b0f26a62d48a5adfdf5f253dfd931c5fa7745d24
Dmitry Kouznetsov <dmitry.kouznetsov@nentgroup.net>
2020-10-17 18:05:45 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+'use strict';
+
+// TODOdd: type this function
+// TODOdd: refactor this function
+module.exports = (entries, {entriesAreCompleteWithoutMedia} = {}) => {
+	if (entriesAreCompleteWithoutMedia === true) {
+		return {completeEntries: entries, incompleteEntries: []};
+	}
+
+	return entries.reduce(
+		(acc, entry) => {
+			if (Array.isArray(entry.media)) {
+				if (entry.media.length > 0) {
+					acc.completeEntries.push(entry);
+				} else {
+					acc.incompleteEntries.push(entry);
+				}
+
+				return acc;
+			}
+
+			if (Object.keys(entry.media || {}).length > 0) {
+				acc.completeEntries.push(entry);
+			} else {
+				acc.incompleteEntries.push(entry);
+			}
+
+			return acc;
+		},
+		{completeEntries: [], incompleteEntries: []}
+	);
+};
